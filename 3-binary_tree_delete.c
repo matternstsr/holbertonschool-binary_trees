f186734849b7079b4009c45ca922d8b3ec82e3c8
matternstsr <matternstsr@gmail.com>
2023-08-16 14:52:47 -0700

    tryoing_5_and_6

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,30 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left-left node
- * @parent:Pointer
- * @value: Value
- * Return: Node
+ * binary_tree_delete - Delete
+ *
+ * @tree: Pointer
  */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+void binary_tree_delete(binary_tree_t *tree)
 {
-	binary_tree_t *new_left_node;
-
-	if (!parent)
+	if (!tree)
 	{
-		return (NULL);
+		return;
 	}
-	new_left_node = binary_tree_node(parent, value);
-	if (parent->left != NULL)
-	{
-		new_left_node->left = parent->left;
-		/*assigns left chld of parent node to left child of new node*/
-		/*new node take place of original left child of the parent node.*/
-		new_left_node->left->parent = new_left_node;
-		/*assigns new node as parent of original left child of parent node.*/
-		/*orig left chld of par node will be gchild of par node & chld of new node.*/
-	}
-	parent->left = new_left_node;
-	return (new_left_node);
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
 }
--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,30 +1,15 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_insert_left-left node
- * @parent:Pointer
- * @value: Value
- * Return: Node
+ * binary_tree_is_leaf- checks if is leaf
+ * @node: Pointer
+ * Return: Leaf
  */
-
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	binary_tree_t *new_left_node;
+	if (!node)
+		return (0);
+	if (!node->left && !node->right)
+		return (1);
 
-	if (!parent)
-	{
-		return (NULL);
-	}
-	new_left_node = binary_tree_node(parent, value);
-	if (parent->left != NULL)
-	{
-		new_left_node->left = parent->left;
-		/*assigns left chld of parent node to left child of new node*/
-		/*new node take place of original left child of the parent node.*/
-		new_left_node->left->parent = new_left_node;
-		/*assigns new node as parent of original left child of parent node.*/
-		/*orig left chld of par node will be gchild of par node & chld of new node.*/
-	}
-	parent->left = new_left_node;
-	return (new_left_node);
+	return (0);
 }
