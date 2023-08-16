ee837da369294248fd97f31bc37e56c3b0887073
matternstsr <matternstsr@gmail.com>
2023-08-16 12:54:09 -0700

    sending_the_right_just_like_the_left

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,30 +1,29 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left-left node
+ * binary_tree_insert_right-right node
  * @parent:Pointer
  * @value: Value
  * Return: Node
  */
-
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_left_node;
+	binary_tree_t *new_right_node;
 
 	if (!parent)
 	{
 		return (NULL);
 	}
-	new_left_node = binary_tree_node(parent, value);
-	if (parent->left != NULL)
+	new_right_node = binary_tree_node(parent, value);
+	if (parent->right != NULL)
 	{
-		new_left_node->left = parent->left;
-		/*assigns left chld of parent node to left child of new node*/
-		/*new node take place of original left child of the parent node.*/
-		new_left_node->left->parent = new_left_node;
-		/*assigns new node as parent of original left child of parent node.*/
-		/*orig left chld of par node will be gchild of par node & chld of new node.*/
+		new_right_node->right = parent->right;
+		/*assigns right chld of parent node to right child of new node*/
+		/*new node take place of original right child of the parent node.*/
+		new_right_node-right->parent = new_right_node;
+		/*assigns new node as parent of original right child of parent node.*/
+		/*orig right chld of par node will be gchild of par node & chld of new node.*/
 	}
-	parent->left = new_left_node;
-	return (new_left_node);
+	parent->right = new_right_node;
+	return (new_right_node);
 }
