77a1254501625b66e285a3e3b8be63fc94c8594a
matternstsr <matternstsr@gmail.com>
2023-08-16 11:36:13 -0700

    cant_have_the_;_after_the_proto_matt

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,7 +7,7 @@
  * Return: Node
  */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_left_node;
 
