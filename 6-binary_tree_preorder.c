60e78ce6bfc838e3d67a3baeca1bd34b01b466ac
matternstsr <matternstsr@gmail.com>
2023-08-16 15:07:59 -0700

    6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,30 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left-left node
- * @parent:Pointer
- * @value: Value
- * Return: Node
+ * binary_tree_preorder - Preorder recursivly
+ *
+ * @tree: Pointer to the node
+ * @func: function that is called called
+ * Return: no return
  */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	binary_tree_t *new_left_node;
+	if (!tree || !func)
+	{
+		return;
+	}
 
-	if (!parent)
-	{
-		return (NULL);
-	}
-	new_left_node = binary_tree_node(parent, value);
-	if (parent->left != NULL)
-	{
-		new_left_node->left = parent->left;
-		/*assigns left chld of parent node to left child of new node*/
-		/*new node take place of original left child of the parent node.*/
-		new_left_node->left->parent = new_left_node;
-		/*assigns new node as parent of original left child of parent node.*/
-		/*orig left chld of par node will be gchild of par node & chld of new node.*/
-	}
-	parent->left = new_left_node;
-	return (new_left_node);
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
