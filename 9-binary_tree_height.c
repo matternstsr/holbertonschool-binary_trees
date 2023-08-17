3abdeac62ab76f47afdf76d893c6cdd0d84fb88c
matternstsr <matternstsr@gmail.com>
2023-08-17 08:02:26 -0700

    fixed_comments_-_they_were_backwards_lol_at_least_the_code_works

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,12 +15,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (0);/*if tree is empty*/
 	}
-	if (tree->right)/*recursively calculates the height of the left.*/
+	if (tree->right)/*recursively calculates the height of the right.*/
 	{
 		right_side = binary_tree_height(tree->right);
 		right_side = right_side + 1;
 	}
-	if (tree->left)/*recursively calculates the height of the right.*/
+	if (tree->left)/*recursively calculates the height of the left.*/
 	{
 		left_side = binary_tree_height(tree->left);
 		left_side = left_side + 1;
