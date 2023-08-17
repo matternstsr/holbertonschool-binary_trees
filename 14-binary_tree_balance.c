91a3a0078b6258d05adbcec92bafeaf7b85832a7
matternstsr <matternstsr@gmail.com>
2023-08-17 07:20:30 -0700

    tryin_alt_hieght_version_that_didnt_work_on_9

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-#include "9-binary_tree_height.c"
 
 /**
  * binary_tree_balance - Measure load balance
@@ -25,3 +24,25 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 
 /*already made binary_tree_height #9 */
+
+/**
+ * binary_tree_height -function that measures the height of a binary tree
+ * @tree: Is a pointer to the root node
+ * Return: If tree is NULL return 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int left_side_ht, right_side_ht;
+
+	if (!tree)
+		return (0);
+
+	left_side_ht = binary_tree_height(tree->left);
+	right_side_ht = binary_tree_height(tree->right);
+	if (left_side_ht > right_side_ht)
+		return (left_side_ht + 1);
+	else
+		return (right_side_ht + 1);
+/*walla*/
+/* tyring alt version of hieght here instead.....*/
+}
