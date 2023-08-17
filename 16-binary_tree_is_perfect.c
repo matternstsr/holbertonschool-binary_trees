cdbbccaba8f30c554a24772cd6077d68bdf85579
matternstsr <matternstsr@gmail.com>
2023-08-16 21:06:04 -0700

    changed_16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,39 +1,4 @@
 #include "binary_trees.h"
-/**
- * binary_tree_size-measures the size of a binary tree
- * @tree: Is the pointer to the binary tree
- * Return: Tje size of the binary tree
- */
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-	int size_l, size_r, total;
-
-	if (!tree)
-		return (0);
-
-	size_l = binary_tree_size(tree->left);
-	size_r = binary_tree_size(tree->right);
-	total = size_l + size_r;
-	return (total + 1);
-}
-/**
- * binary_tree_is_full-checks if a binary tree is full
- * @tree: Is the pointer to binary tree
- * Return: 1 if is full
- */
-int binary_tree_is_full(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-
-	if (!tree->left && !tree->right)
-		return (1);
-
-	if ((tree->left) && (tree->right))
-		return (binary_tree_is_full(tree->left)
-			&& binary_tree_is_full(tree->right));
-	return (0);
-}
 
 /**
  * binary_tree_is_perfect-function that checks if a binary tree is perfec
@@ -53,4 +18,56 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (binary_tree_is_full(tree) && left_h == right_h)
 		return (1);
 	return (0);
+}
+
+
+
+
+
+
+/*already written functions*/
+
+/**
+ * binary_tree_size - determines size of a the provided tree
+ * @tree: root node? seems different no sure how to explaine
+ * Return: tree's size
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	const binary_tree_t leftnode = NULL, rightnode = NULL;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		leftnode = tree->left;
+	if (tree->right)
+		rightnode = tree->right;
+
+	return (binary_tree_size(leftnode) + 1 + binary_tree_size(rightnode));
+}
+
+
+
+
+/**
+ * binary_tree_is_full-checks if the tree is full of kiddos
+ * @tree: points to tree
+ * Return: 1 if is full
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)/*not a tree get out*/
+		return (0);
+
+	if (!tree->left && !tree->right) /*if no kiddos return that its full*/
+		return (1);
+
+	if ((tree->left) && (tree->right))/* if you have kiddos on both*/
+		return (binary_tree_is_full(tree->left)/* recur till complete*/
+			&& binary_tree_is_full(tree->right));/* recur till complete*/
+	return (0);
+/*walla*/
 }
