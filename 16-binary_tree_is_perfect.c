71ee01dd91577d73798e44e23db95fc801196f94
matternstsr <matternstsr@gmail.com>
2023-08-16 22:49:01 -0700

    fixed_16-binary_tree_is_perfect_pointers_were_missing

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -35,19 +35,19 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	const binary_tree_t leftnode = NULL, rightnode = NULL;
+	const binary_tree_t *leftnode = NULL, *rightnode = NULL;
 
-	if (!tree)
+	if (!tree)/*if not tree get out*/
 		return (0);
 
-	if (tree->left)
+	if (tree->left)/*if there is a left child move ptr*/
 		leftnode = tree->left;
-	if (tree->right)
+	if (tree->right)/*if there is a right child move ptr*/
 		rightnode = tree->right;
 
 	return (binary_tree_size(leftnode) + 1 + binary_tree_size(rightnode));
+/*walla*/
 }
-
 
 
 
@@ -70,5 +70,4 @@ int binary_tree_is_full(const binary_tree_t *tree)
 			&& binary_tree_is_full(tree->right));/* recur till complete*/
 	return (0);
 /*walla*/
-/**leftnode*rightnode*/
 }
