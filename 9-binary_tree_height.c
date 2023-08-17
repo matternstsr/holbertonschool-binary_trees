d4fd5d4878d73f49183044a165b2af877afd13c8
matternstsr <matternstsr@gmail.com>
2023-08-17 08:42:04 -0700

    trying_9_with_no_spaces

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,31 +8,31 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_side = 0;
-    size_t right_side = 0;
+	size_t left_side = 0;
+	size_t right_side = 0;
 
-    if (!tree)
-    {
-        return (0);
-    }
+	if (!tree)
+	{
+		return (0);
+	}
 
-    if (tree->left)
-    {
-        left_side = binary_tree_height(tree->left);
-        left_side = left_side + 1;
-    }
-    if (tree->right)
-    {
-        right_side = binary_tree_height(tree->right);
-        right_side = right_side + 1;
-    }
+	if (tree->left)
+	{
+		left_side = binary_tree_height(tree->left);
+		left_side = left_side + 1;
+	}
+	if (tree->right)
+	{
+		right_side = binary_tree_height(tree->right);
+		right_side = right_side + 1;
+	}
 
-    if (left_side > right_side)
-    {
-        return (left_side);
-    }
-    else
-    {
-        return (right_side);
-    }
+	if (left_side > right_side)
+	{
+		return (left_side);
+	}
+	else
+	{
+		return (right_side);
+	}
 }
