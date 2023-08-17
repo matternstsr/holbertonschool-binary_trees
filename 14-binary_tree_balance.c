c6668f15ee1a6da4136ec77e66b140856c287b8e
matternstsr <matternstsr@gmail.com>
2023-08-16 22:35:09 -0700

    corrected_tabbing_on_14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -34,31 +34,30 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t left_side = 0;
-        size_t right_side = 0;
+	size_t left_side = 0;
+	size_t right_side = 0;
 
-        if (!tree)
-        {
-                return (0);/*if tree is empty*/
-        }
-        if (tree->left)/*recursively calculates the height of the left.*/
-        {
-                left_side = binary_tree_height(tree->left);
-                left_side = left_side + 1;
-        }
-        if (tree->right)/*recursively calculates the height of the right.*/
-        {
-                right_side = binary_tree_height(tree->right);
-                right_side = right_side + 1;
-        }
-
-        if (left_side > right_side)/*return the greater side*/
-        {
-                return (left_side);
-        }
-        else
-        {
-                return (right_side);
-        }
-        /*walla*/
+	if (!tree)
+	{
+		return (0);/*if tree is empty*/
+	}
+	if (tree->left)/*recursively calculates the height of the left.*/
+	{
+		left_side = binary_tree_height(tree->left);
+		left_side = left_side + 1;
+	}
+	if (tree->right)/*recursively calculates the height of the right.*/
+	{
+		right_side = binary_tree_height(tree->right);
+		right_side = right_side + 1;
+	}
+	if (left_side > right_side)/*return the greater side*/
+	{
+		return (left_side);
+	}
+	else
+	{
+		return (right_side);
+	}
+/*walla*/
 }
