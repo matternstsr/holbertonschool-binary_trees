0fb13986f63d2a2b936ae53a3b4a018a12770902
matternstsr <matternstsr@gmail.com>
2023-08-16 22:28:28 -0700

    updated_11_with_ptr_for_the_l&r_node_position

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,17 +8,16 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	const binary_tree_t leftnode = NULL, rightnode = NULL;
+	const binary_tree_t *leftnode = NULL, *rightnode = NULL;
 
-	if (!tree)
+	if (!tree)/*if not tree get out*/
 		return (0);
 
-	if (tree->left)
+	if (tree->left)/*if there is a left child move ptr*/
 		leftnode = tree->left;
-	if (tree->right)
+	if (tree->right)/*if there is a right child move ptr*/
 		rightnode = tree->right;
 
 	return (binary_tree_size(leftnode) + 1 + binary_tree_size(rightnode));
 /*walla*/
-/**leftnode*rightnode*/
 }
