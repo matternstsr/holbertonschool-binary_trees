b7cc7386d1ca7f0320487cd04f061d27666092a3
matternstsr <matternstsr@gmail.com>
2023-08-16 22:20:35 -0700

    twsp6&7

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,8 +3,8 @@
 /**
  * binary_tree_height - height of the tree
  * @tree: root node (pointer to tree)
- * Return: height# 
- */ 
+ * Return: height#
+ */
 
 
 size_t binary_tree_height(const binary_tree_t *tree)
