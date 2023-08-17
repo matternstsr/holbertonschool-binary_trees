8a3b8703b17298d829037c10341be3dae58966a7
matternstsr <matternstsr@gmail.com>
2023-08-16 22:45:45 -0700

    right_and_left_were_not_correct_on_14_and_9

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -28,36 +28,39 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 /**
  * binary_tree_height - height of the tree
- * @tree: root node (tree pointer)
- * Return: height number
+ * @tree: root node (pointer to tree)
+ * Return: height#
  */
+
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_side = 0;
-	size_t right_side = 0;
+        size_t left_side = 0;
+        size_t right_side = 0;
 
-	if (!tree)
-	{
-		return (0);/*if tree is empty*/
-	}
-	if (tree->left)/*recursively calculates the height of the left.*/
-	{
-		left_side = binary_tree_height(tree->left);
-		left_side = left_side + 1;
-	}
-	if (tree->right)/*recursively calculates the height of the right.*/
-	{
-		right_side = binary_tree_height(tree->right);
-		right_side = right_side + 1;
-	}
-	if (left_side > right_side)/*return the greater side*/
-	{
-		return (left_side);
-	}
-	else
-	{
-		return (right_side);
-	}
+        if (!tree)
+        {
+                return (0);/*if tree is empty*/
+        }
+        if (tree->right)/*recursively calculates the height of the right.*/
+        {
+                right_side = binary_tree_height(tree->right);
+                right_side = right_side + 1;
+        }
+        if (tree->left)/*recursively calculates the height of the left.*/
+        {
+                left_side = binary_tree_height(tree->left);
+                left_side = left_side + 1;
+        }
+
+        if (left_side > right_side)/*return the greater side*/
+        {
+                return (left_side);
+        }
+        else
+        {
+                return (right_side);
+        }
 /*walla*/
 }
+
