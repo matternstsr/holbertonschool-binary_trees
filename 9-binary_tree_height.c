4fc8932bc2bd357f1b7c406b6777348f58a0bf94
matternstsr <matternstsr@gmail.com>
2023-08-17 08:40:52 -0700

    trying_new_9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,38 +1,38 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - height of the tree
- * @tree: root node (pointer to tree)
- * Return: height#
+ * binary_tree_height -function that measures the height of a binary tree
+ * @tree: Is a pointer to the root node
+ * Return: If tree is NULL return 0
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_side = 0;
-	size_t right_side = 0;
+    size_t left_side = 0;
+    size_t right_side = 0;
 
-	if (!tree)
-	{
-		return (0);/*if tree is empty*/
-	}
-	if (tree->right)/*recursively calculates the height of the right.*/
-	{
-		right_side = binary_tree_height(tree->right);
-		right_side = right_side + 1;
-	}
-	if (tree->left)/*recursively calculates the height of the left.*/
-	{
-		left_side = binary_tree_height(tree->left);
-		left_side = left_side + 1;
-	}
+    if (!tree)
+    {
+        return (0);
+    }
 
-	if (left_side > right_side)/*return the greater side*/
-	{
-		return (left_side);
-	}
-	else
-	{
-		return (right_side);
-	}
-/*walla*/
+    if (tree->left)
+    {
+        left_side = binary_tree_height(tree->left);
+        left_side = left_side + 1;
+    }
+    if (tree->right)
+    {
+        right_side = binary_tree_height(tree->right);
+        right_side = right_side + 1;
+    }
+
+    if (left_side > right_side)
+    {
+        return (left_side);
+    }
+    else
+    {
+        return (right_side);
+    }
 }
