36b2657d0c601e585af0bbd1b57a084d8d718e60
matternstsr <matternstsr@gmail.com>
2023-08-16 21:38:06 -0700

    updates

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_depth-function that measures the how deep a binary tree is
- * @tree: Is the ponter to the binary tree 
+ * @tree: Is the ponter to the binary tree
  * Return: how deep the binary tree is
  */
 
@@ -12,7 +12,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 
 	if (!tree || !tree->parent)/*there is a tree and parrent*/
 		return (0);
-	/*function is recursively called on each parent node until it reaches the root node.*/
+	/*func is recur called on each par node until it reaches the root node.*/
 	depth = binary_tree_depth(tree->parent);
 	return (depth + 1);/*value is added to 1 to account for the current node.*/
 }
